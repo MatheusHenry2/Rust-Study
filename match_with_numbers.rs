aa4a8563c3b4d60b265bbe20d11f6a2c4ecd4199
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-02-06 11:20:00 -0300

    Add files via upload

---
+++ match_with_numbers.rs
@@ -0,0 +1,13 @@
+fn main() {
+    
+    let b: i32 = 4;
+    
+    match b{
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        _ => println!("other number"),
+    }
+    
+}
