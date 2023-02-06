7838dadaa94570c7126424a6ae1656b9e667833c
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-02-06 09:34:18 -0300

    Add files via upload

---
+++ number_if_and_else_size.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number: i32 = 7;
+
+    if number > 5 {
+        println!("{} 5 >", number);
+    } else if number < 5 {
+        println!("{} 5 < ", number);
+    } else {
+        println!("{} 5 == ", number);
+    }
+}
