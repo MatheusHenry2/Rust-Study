0345aa31535a1c8d9fab37da2cd057c0f94e8e9a
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-02-06 10:22:37 -0300

    Add files via upload

---
+++ switch_case.rs
@@ -0,0 +1,12 @@
+fn main() {
+
+    let some_int = 1;
+    
+    match some_int{
+        1 => println!("its 1"),
+        2 => println!("its 2"),
+        3 => println!("its 3"),
+        _ => println!("is something else"),
+
+    }
+}
