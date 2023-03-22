d6710d8a0427f0ea4c6f4f7bc14603b0c75466f9
MatheusHenry2 <71864649+MatheusHenry2@users.noreply.github.com>
2023-03-22 19:27:02 -0300

    Add files via upload

---
+++ tuples_example.rs
@@ -0,0 +1,20 @@
+
+
+fn main() {
+   
+    let coord = (2,3);
+    println!("{:?}, {:?}", coord.0, coord.1);
+    
+    let (x, y) = (2, 3);
+
+    println!("{:?}, {:?}", x, y);
+
+    let (name, age) = ("Emma", 20);
+
+    let favorites = ("red", 14, "TX", "pizza", "tv show", "home");
+
+    let state = favorites.2;
+    let place = favorites.5;
+
+}
+
